11607701032ffcc388da2c0c59aaacce51a9aef6
wwwrwww65-netizen <wwwrwww65-netizen@users.noreply.github.com>
2026-08-31 10:13:12 +0300

    النسخه النهائيه من شبكه تونس واضافه ملف شبكه جديده

---
+++ Hotspot2010/config/config.js
@@ -0,0 +1,107 @@
+﻿Config({
+    "network-name": "  المحمدي نت",
+    "service-number": "782933222",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "User",
+    "login-chap": 0,
+    "news-line": " اليكم البث المباشر مباريات كأس العالم على الرابط a.com  ",
+    "input-type": "tel",
+    "input-autocomplete": "on",
+    "speed-trychange": 3 ,//عدد مرات هلاح التغيير
+    "speed-trychange-timeout": 3 ,// مدة فترة  حضر التغيير بالدقائق
+    "input-rm-white-spaces": 0,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 20,
+    "warn-when": 10,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+	"redirect-to-esterahah": "http://f.com/fiberlive/live.html",
+    "redirect-to-mobasher": "http://f.com/fiberlive/live.html",
+    "show-date-field": false,
+    "loan-button": true,
+    "login-speeds-mode": true,
+    "redirect-to-esterahah": "",
+    "redirect-to-mobasher": "",
+    "app-store-base-url": "",
+    "app-store-base-url-ext": "",
+   "profiles": [
+        {
+            "price": "200 ريال",
+            "time": "7 ساعات ",
+            "transfer": "900 ميجا بايت",
+            "validity": "5 أيام",
+        },
+	{
+            "price": "300 ريال",
+            "time": "10 ساعات ",
+            "transfer": "1000 ميجا",
+            "validity": "10 أيام",
+        },
+     
+        {
+            "price": "500 ريال",
+            "time": "14 ساعة ",
+            "transfer": "2000 ميجا",
+            "validity": "12 يوم",
+        },
+        {
+            "price": "1000 ريال",
+            "time": "الساعات مفتوح ",
+            "transfer": "4000 ميجا بايت",
+            "validity": "15 أيام",
+        },
+        {
+            "price": "3000 ريال",
+            "time": "الساعات مفتوح ",
+            "transfer": "15 جيجا بايت",
+            "validity": "25 يوم",
+        },
+      
+      
+        
+        
+    ],
+    "sell-points": [
+        {"name":" "},
+        {"name":" "},
+        {"name":""},
+        {"name":""},
+        {"name":""},
+        {"name":" "},
+        {"name":" "},
+        {"name":" "},
+        {"name":" "},
+        {"name":" "},
+        {"name":" "},
+        {"name":" "},
+        {"name":"جميع البقالات والمراكز والتموينات المجاورة للشبكة"},
+        بقاله البركه
+        
+    ]
+})
+
+
+
+
+
+
+
+
+
+
+
